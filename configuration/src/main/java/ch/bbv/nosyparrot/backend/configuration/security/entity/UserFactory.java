5c2666b01c90e3220280f295d2fb10b80e92c7cf
Sven Osterwalder <sven.osterwalder@bbv.ch>
2017-10-30 13:17:30 +0100

    Provide UserFactory

---
+++ configuration/src/main/java/ch/bbv/nosyparrot/backend/configuration/security/entity/UserFactory.java
@@ -0,0 +1,16 @@
+package ch.bbv.nosyparrot.backend.configuration.security.entity;
+
+import ch.bbv.nosyparrot.backend.core.entity.User;
+
+public class UserFactory implements ch.bbv.nosyparrot.backend.core.entity.UserFactory{
+    private UserFactory() {}
+
+    @Override
+    public User createUser(String username, String password) {
+        return new ch.bbv.nosyparrot.backend.configuration.security.entity.User(username, password);
+    }
+
+    public static UserFactory create() {
+        return new UserFactory();
+    }
+}
