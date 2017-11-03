f933fd965496de49da414c1941856a7fa2091973
Sven Osterwalder <sven.osterwalder@gmail.com>
2017-11-03 11:35:28 +0100

    Rename test files for usecases layer

---
+++ usecases/src/test/java/ch/bbv/nosyparrot/backend/usecases/step_definitions/ListSurveysTestRunner.java
@@ -0,0 +1,9 @@
+Unstaged changes after reset:
+M	interfaces/build.gradle
+M	interfaces/src/main/java/ch/bbv/nosyparrot/backend/interfaces/ListSurveysPresenter.java
+M	interfaces/src/test/resources/features/ListSurveys.feature
+M	settings.gradle
+D	usecases/src/test/java/ch/bbv/nosyparrot/backend/usecases/step_definitions/ListSurveys.java
+M	usecases/src/test/java/ch/bbv/nosyparrot/backend/usecases/step_definitions/ListSurveysSteps.java
+D	usecases/src/test/java/ch/bbv/nosyparrot/backend/usecases/step_definitions/ListSurveysTest.java
+M	usecases/src/test/java/ch/bbv/nosyparrot/backend/usecases/step_definitions/ListSurveysTestRunner.java
