e2cf5496fad5f75812d3cb633f5c1469b00fcceb
Sven Osterwalder <sven.osterwalder@bbv.ch>
2017-11-13 10:42:41 +0100

    Implement user factory

--- core/src/main/java/ch/bbv/nosyparrot/backend/core/entity/UserFactory.java
+++ core/src/main/java/ch/bbv/nosyparrot/backend/core/entity/UserFactory.java
@@ -1,5 +1,16 @@
 package ch.bbv.nosyparrot.backend.core.entity;
 
-public interface UserFactory {
-    User createUser(String username, String password);
+public class UserFactory {
+    private UserFactory() {}
+
+    public User createUser(long id, String username, String password) {
+        User user = new User(username, password);
+        user.setId(id);
+
+        return user;
+    }
+
+    public static UserFactory create() {
+        return new UserFactory();
+    }
 }
